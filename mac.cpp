2fad4a7d7b103660777060a8659444c361d4d032
tuyulvalo1 <tuyulvalo1@gmail.com>
2025-09-09 04:57:40 +0000

    add cpp file

---
+++ mac.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "test\n"; 
+    __asm__ = {
+                mov rax, 1
+                mov rdi, 0
+                mov rsi, 0x80
+                
+    }
+}
